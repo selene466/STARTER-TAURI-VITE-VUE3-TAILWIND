c250eb77ca27d37fa2d35c7b2c3a3827363d58f0
selene466 <selene.banderas.466@gmail.com>
2024-06-15 18:25:07 +0700

    improve dev mode

--- vite.config.js
+++ vite.config.js
@@ -8,67 +8,72 @@ import IconsResolver from "unplugin-icons/resolver";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    VueRouter({
-      routesFolder: [
-        {
-          src: "src/pages",
-          path: "/",
-        },
-      ],
-    }),
-    vue({
-      template: {
-        transformAssetUrls: {
-          base: null,
-          includeAbsolute: false,
-        },
-      },
-    }),
-    Components({
-      dirs: ["src/components", "src/layouts"],
-      resolvers: [IconsResolver({ prefix: "icons" })],
-    }),
-    Icons({ autoInstall: true, compiler: "vue3" }),
-    VitePWA({
-      /* comment devOptions below for production */
-      devOptions: {
-        enabled: true,
-        type: "module",
-      },
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
-      manifest: {
-        name: "Starter Vite + Vue",
-        short_name: "StarterApp",
-        description: "Some description",
-        theme_color: "#ffffff",
-        icons: [
+export default defineConfig(({ mode }) => {
+  const isDevelopment = mode === "development";
+
+  return {
+    plugins: [
+      VueRouter({
+        routesFolder: [
           {
-            src: "android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
+            src: "src/pages",
+            path: "/",
           },
         ],
+      }),
+      vue({
+        template: {
+          transformAssetUrls: {
+            base: null,
+            includeAbsolute: false,
+          },
+        },
+      }),
+      Components({
+        dirs: ["src/components", "src/layouts"],
+        resolvers: [IconsResolver({ prefix: "icons" })],
+      }),
+      Icons({ autoInstall: true, compiler: "vue3" }),
+      VitePWA({
+        ...(isDevelopment && {
+          devOptions: {
+            enabled: true,
+            type: "module",
+          },
+        }),
+        includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+        manifest: {
+          name: "Starter Vite + Vue",
+          short_name: "StarterApp",
+          description: "Some description",
+          theme_color: "#ffffff",
+          icons: [
+            {
+              src: "android-chrome-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "android-chrome-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
+        registerType: "autoUpdate",
+        injectRegister: "script-defer",
+        workbox: {
+          clientsClaim: true,
+          skipWaiting: true,
+          globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        vue: "vue/dist/vue.esm-bundler.js",
+        "@": path.resolve(__dirname, "./src"),
       },
-      registerType: "autoUpdate",
-      injectRegister: "script-defer",
-      workbox: {
-        clientsClaim: true,
-        skipWaiting: true,
-        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      vue: "vue/dist/vue.esm-bundler.js",
-      "@": path.resolve(__dirname, "./src"),
     },
-  },
+  };
 });
